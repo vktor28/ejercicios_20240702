e0be3f32f4e459a2204c1b4ad43b4bb4f00334c0
vktor28 <167107545+vktor28@users.noreply.github.com>
2024-07-02 20:55:05 +0200

    Add files via upload

---
+++ src/main/java/com/example/gestorconcesionarios/Gestorconcesionarios2Application.java
@@ -0,0 +1,13 @@
+package com.example.gestorconcesionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gestorconcesionarios2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gestorconcesionarios2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestorconcesionarios/Gestorconcesionarios2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestorconcesionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gestorconcesionarios2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
